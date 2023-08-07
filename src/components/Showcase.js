445ffed3bd0d9fed7ff208163518cd7bab8a744c
kenliii <90875261+kenliii@users.noreply.github.com>
2023-08-07 07:31:48 -0700

    working

--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -1,6 +1,26 @@
+import { useState } from "react"
 import React from 'react'
 
+const data = [
+    {
+        id: 1,
+        title: "Discover innovative ways to decorate",
+        desc: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+    },
+    {
+        id: 1,
+        title: "Discover innovative ways to decorate",
+        desc: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+    },
+    {
+        id: 1,
+        title: "Discover innovative ways to decorate",
+        desc: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
+    }
+]
+
 export default function Showcase() {
+    const [items] =useState(data)
   return (
     <>
         <h1>Showcase</h1>
