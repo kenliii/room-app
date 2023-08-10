30b9713ef222a19fe872039588bf9f2e3445312b
kenliii <90875261+kenliii@users.noreply.github.com>
2023-08-10 01:24:19 -0700

    sectiontwo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Header from "./components/header";
-import Showcase from "./components/Showcase";
+import Header from "./components/header"
+import Showcase from "./components/Showcase"
+import SectionTwo from "./components/SectionTwo"
 
 function App() {
   return (
     <>
       <Header />
       <Showcase />
+      <SectionTwo />
     </>
   
   );
